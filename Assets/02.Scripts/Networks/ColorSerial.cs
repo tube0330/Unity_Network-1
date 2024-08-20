208c26c4b4e9a24f6bf32ac91eff438cb7644942
tube0330 <zien0516@naver.com>
2024-08-20 12:07:44 +0900

    240820

--- Assets/02.Scripts/Networks/ColorSerial.cs
+++ Assets/02.Scripts/Networks/ColorSerial.cs
@@ -5,9 +5,9 @@ using ExitGames.Client.Photon;
 
 public class ColorSerial : MonoBehaviour
 {
-    public static byte[] SerializeColor(object targetObj)
+    /* public static byte[] SerializeColor(object targetObj)
     {
         Color color = (Color)targetObj;
         Quaternion colorQuaternion = new Quaternion(color.r, color.g, color.b, color.a);
-    }
+    } */
 }
